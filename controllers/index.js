a83b9b41f2eaf29afcf3e32074db8d01f5bcc90a
Nefi Perez <nefipr@gmail.com>
2023-09-21 20:17:45 +0200

    Create Controllers

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+export const EmaRoute = (req, res) => {
+    res.send('Ema Esteves');
+};
+
+export const NefiRoute = (req, res) => {
+    res.send('Nefi Perez');
+};
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const controllers = require('./controllers/index.js');
+
+const routes = require('express').Router()
  
-app.get('/', (req, res) => {
-  res.send("Nefi Perez");
-});
+app.get('/', 'controllers.EmaRoute');
+
+app.get('/nefi', 'controllers.NefiRoute');
  
 app.listen(process.env.PORT || 3000, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
